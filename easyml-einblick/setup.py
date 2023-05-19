8c8851ea2d7d22f5c9c3075bfc58462533222228
paul <111363033+pyeinblick@users.noreply.github.com>
2023-05-19 15:18:45 -0400

    Update setup.py

--- easyml-einblick/setup.py
+++ easyml-einblick/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='easyml-einblick',
     version='1.0',
-    py_modules=['easyml-einblick'],
+    py_modules=['easyml_einblick'],
     install_requires=[
         'tpot','xgboost','shap'  # Add any dependencies required by your class
     ],
