61e3819b9bcc36b8fd20934e3407fb71d3a4c627
paul <111363033+pyeinblick@users.noreply.github.com>
2023-07-11 14:30:19 -0400

    Create setup.py

---
+++ describetwo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='easydescribe_einblick',
+    version='1.0',
+    py_modules=['easydescribe_einblick'],
+    install_requires=[
+        'tabulate','pandas'  # Add any dependencies required by your class
+    ],
+)
